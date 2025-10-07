b3655762ba2b8eaf90d25884d88aab7cb420445a
Ondřej Pavlín <pavlio12@fel.cvut.cz>
2025-10-07 17:24:45 +0200

    Working LCD display as in e205485. Will add USB

--- KRP_HMI_02/CM7/Core/Src/stm32h7xx_hal_msp.c
+++ KRP_HMI_02/CM7/Core/Src/stm32h7xx_hal_msp.c
@@ -404,11 +404,11 @@ void HAL_LTDC_MspInit(LTDC_HandleTypeDef* hltdc)
     PeriphClkInitStruct.PLL3.PLL3M = 5;
     PeriphClkInitStruct.PLL3.PLL3N = 161;
     PeriphClkInitStruct.PLL3.PLL3P = 2;
-    PeriphClkInitStruct.PLL3.PLL3Q = 8;
+    PeriphClkInitStruct.PLL3.PLL3Q = 17;
     PeriphClkInitStruct.PLL3.PLL3R = 21;
     PeriphClkInitStruct.PLL3.PLL3RGE = RCC_PLL3VCIRANGE_2;
     PeriphClkInitStruct.PLL3.PLL3VCOSEL = RCC_PLL3VCOWIDE;
-    PeriphClkInitStruct.PLL3.PLL3FRACN = 0.0;
+    PeriphClkInitStruct.PLL3.PLL3FRACN = 0;
     if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK)
     {
       Error_Handler();
